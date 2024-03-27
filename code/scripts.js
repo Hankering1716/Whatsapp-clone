01b61def9f8ac5b3350ea6cd2f27a5f7465afe77
Hankering1716 <116718014+Hankering1716@users.noreply.github.com>
2024-03-27 21:45:26 +0100

    Update scripts.js

--- code/scripts.js
+++ code/scripts.js
@@ -2,14 +2,18 @@ function sendMessage() {
     if(document.getElementById('inputSender').value =="") {
         return  document.getElementById('conversationSpeaker').innerHTML += ''
     }
-    document.getElementById('conversationSpeaker').innerHTML += `                        
-    
-        <div class="d-flex flex-row  p-3 m-4  border border-bg-primary text-dark" style="border-radius: 15px; background-color: rgb(49, 49, 175);">
-              <p class="small mb-1">${document.getElementById('inputSender').value}</p>
-        </div>`;
-        document.getElementById('inputSender').value = ""
 
-        window.scrollTo(0, document.body.scrollHeight);
+
+
+    document.getElementById('conversationSpeaker').innerHTML += `                        
+        <div class="d-flex flex-row  p-3 m-4  border border-bg-primary text-white" style="border-radius: 15px; background-color: rgb(49, 49, 175);">
+              <p class="small mb-1">${document.getElementById('inputSender').value} <img src="${document.getElementById("imagePreview")}"</p>
+        </div>`;
+
+        
+    document.getElementById('inputSender').value = ""
+
+    window.scrollTo(0, document.body.scrollHeight);
 
         
 
@@ -27,24 +31,11 @@ document.addEventListener('keydown',event => {
 
 function loadImage() {
 
-    var preview = document.getElementById('img');
-    var file    = document.querySelector('input[type=file]').files[0];
-    var reader  = new FileReader();
-  
-    reader.onloadend = function () {
-      preview.src = reader.result;
-    }
-  
-    if (file) {
-      reader.readAsDataURL(file);
-    } else {
-      preview.src = "";
-    }
-  
     document.getElementById('conversationSpeaker').innerHTML += 
-`<div class="d-flex flex-row  p-3 m-4  border border-bg-primary text-dark" style="border-radius: 15px; background-color: rgb(49, 49, 175);">
-              <p class="small mb-1">${document.getElementById('file').preview}</p>
+        `<div class="d-flex flex-row  p-3 m-4  border border-bg-primary text-white" style="border-radius: 15px;color:white; background-color: rgb(49, 49, 175);">
+              <img class="small mb-1 rounded" src="${document.getElementById("file-input").value}" alt='Image'">
         </div>`;
 
-        document.getElementById('file').value = "";
+   
 }
+
